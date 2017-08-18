bb865a73e2b0b40d92c71f9ab26c87cdc7216724
Sebastian Sierra <sebastian.sierra1993@gmail.com>
2017-08-18 14:52:36 -0500

    images added

--- scripts/app.js
+++ scripts/app.js
@@ -168,8 +168,6 @@
 
     };
 
-    // TODO uncomment line below to test app with fake data
-    // app.updateForecastCard(initialWeatherForecast);
 
     /************************************************************************
      *
@@ -181,33 +179,6 @@
      *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
      *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
      ************************************************************************/
-
-    // TODO add startup code here
-    /*app.selectedCities = localStorage.selectedCities;
-     if (app.selectedCities) {
-     app.selectedCities = JSON.parse(app.selectedCities);
-     app.selectedCities.forEach(function(city) {
-     app.getForecast(city.key, city.label);
-     });
-     } else {
-     /!* The user is using the app for the first time, or the user has not
-     * saved any cities, so show the user some fake data. A real app in this
-     * scenario could guess the user's location via IP lookup and then inject
-     * that data into the page.
-     *!/
-     app.updateForecastCard(initialWeatherForecast);
-     app.selectedCities = [
-     {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
-     ];
-     app.saveSelectedCities();
-     }
-
-     // TODO add service worker code here
-     if ('serviceWorker' in navigator) {
-     navigator.serviceWorker
-     .register('./service-worker.js')
-     .then(function() { console.log('Service Worker Registered'); });
-     }*/
 
     app.getSchedule('metros/1/bastille/A', 'Bastille, Direction La Défense');
     app.selectedTimetables = [
